8909a13701441613b83022fbad41130bc4a8db0f
Kratso <kratsothelsamar@gmail.com>
2018-09-20 10:02:16 +0200

    3.2

---
+++ BuscadorFicheros/src/buscador2/BuscadorArchivos2.java
@@ -0,0 +1,206 @@
+package buscador2;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.awt.event.ActionEvent;
+
+public class BuscadorArchivos2 extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField pathText;
+	private JTextField textField;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JTextArea textArea;
+	private JRadioButton rdbtnMayor;
+	private JCheckBox chckbxOcultos;
+	private JCheckBox chckbxIncluirSubcarpetas;
+	private File file;
+	private JButton btnBuscar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BuscadorArchivos2 frame = new BuscadorArchivos2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public BuscadorArchivos2() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 603, 326);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JButton seleccionCarpeta = new JButton("Seleccionar Carpeta");
+		seleccionCarpeta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser jfc = new JFileChooser("/home/alumno");
+				jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int opcion = jfc.showOpenDialog(null);
+				if (opcion == JFileChooser.APPROVE_OPTION) {
+					file = jfc.getSelectedFile();
+					pathText.setText(file.getAbsolutePath());
+					btnBuscar.setEnabled(true);
+				}
+
+			}
+		});
+		seleccionCarpeta.setBounds(0, 6, 187, 27);
+		contentPane.add(seleccionCarpeta);
+
+		pathText = new JTextField();
+		pathText.setEditable(false);
+		pathText.setBounds(206, 6, 378, 27);
+		contentPane.add(pathText);
+		pathText.setColumns(10);
+
+		JLabel lblTamao = new JLabel("DD-MM-AAAA");
+		lblTamao.setBounds(6, 59, 123, 15);
+		contentPane.add(lblTamao);
+
+		textField = new JTextField();
+		textField.setBounds(92, 53, 100, 27);
+		contentPane.add(textField);
+		textField.setColumns(10);
+
+		JRadioButton rdbtnMenor = new JRadioButton("DESPUÉS DE");
+		buttonGroup.add(rdbtnMenor);
+		rdbtnMenor.setBounds(206, 87, 113, 18);
+		contentPane.add(rdbtnMenor);
+
+		rdbtnMayor = new JRadioButton("ANTES DE");
+		buttonGroup.add(rdbtnMayor);
+		rdbtnMayor.setBounds(206, 57, 113, 18);
+		contentPane.add(rdbtnMayor);
+		rdbtnMayor.setSelected(true);
+
+		chckbxOcultos = new JCheckBox("Mostrar Ocultos");
+		chckbxOcultos.setBounds(331, 57, 150, 18);
+		contentPane.add(chckbxOcultos);
+
+		chckbxIncluirSubcarpetas = new JCheckBox("Incluir Subcarpetas");
+		chckbxIncluirSubcarpetas.setBounds(331, 87, 248, 18);
+		contentPane.add(chckbxIncluirSubcarpetas);
+
+		btnBuscar = new JButton("BUSCAR");
+		btnBuscar.setEnabled(false);
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				long fecha;
+				SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-YYYY");
+				sdf.setLenient(false);
+
+				boolean antesDe = rdbtnMayor.isSelected();
+				try {
+					fecha = sdf.parse(textField.getText()).getTime();
+					ArrayList<File> encuentros = buscarArchivosPorFechaModif(file, fecha, antesDe,
+							chckbxOcultos.isSelected(), chckbxIncluirSubcarpetas.isSelected());
+					String texto = "";
+					for (File f : encuentros) {
+						texto += f.getPath() + "(Última modificación: " + sdf.format(new Date(f.lastModified()))
+								+ ")\n";
+					}
+					textArea.setText(texto);
+				} catch (ParseException pe) {
+					JOptionPane.showMessageDialog(null,
+							"Tamaño no válido\n\n\n" + pe.getClass() + " " + pe.getMessage());
+				}
+			}
+
+		});
+		btnBuscar.setBounds(484, 59, 100, 27);
+		contentPane.add(btnBuscar);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 135, 589, 159);
+		contentPane.add(scrollPane);
+
+		textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		textArea.setEditable(false);
+	}
+
+	protected ArrayList<File> buscarArchivosPorFechaModif(File carpetaDondeBuscar, long fecha, boolean antesDE,
+			boolean incluirOcultos, boolean incluirSubcarpetas) {
+		if (!carpetaDondeBuscar.isDirectory())
+			return null;
+		Date date = new Date(fecha);
+		ArrayList<File> resul = new ArrayList<>();
+		for (File f : carpetaDondeBuscar.listFiles()) {
+			if (f.isFile()) {
+				if ((f.isHidden() && incluirOcultos) || (!f.isHidden())) {
+					if (antesDE && (date.compareTo(new Date(f.lastModified())) > 0))
+						resul.add(f);
+					else if (!antesDE && (date.compareTo(new Date(f.lastModified())) < 0))
+						resul.add(f);
+
+				}
+			} else {
+				if ((f.isDirectory() && incluirSubcarpetas) && ((f.isHidden() && incluirOcultos) || (!f.isHidden())))
+					resul.addAll(buscarArchivosPorFechaModif(f, fecha, antesDE, incluirOcultos, incluirSubcarpetas));
+			}
+		}
+		return resul;
+	}
+
+	public JTextArea getTextArea() {
+		return textArea;
+	}
+
+	public JRadioButton getRdbtnMayor() {
+		return rdbtnMayor;
+	}
+
+	public JTextField getTextField() {
+		return textField;
+	}
+
+	public JTextField getPathText() {
+		return pathText;
+	}
+
+	public JCheckBox getChckbxOcultos() {
+		return chckbxOcultos;
+	}
+
+	public JCheckBox getChckbxIncluirSubcarpetas() {
+		return chckbxIncluirSubcarpetas;
+	}
+
+	public JButton getBtnBuscar() {
+		return btnBuscar;
+	}
+}
---
+++ BuscadorFicheros/src/buscador3/BuscadorArchivos3.java
@@ -0,0 +1,180 @@
+package buscador3;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.regex.PatternSyntaxException;
+import java.awt.event.ActionEvent;
+
+public class BuscadorArchivos3 extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField pathText;
+	private JTextField textField;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JTextArea textArea;
+	private JCheckBox chckbxOcultos;
+	private JCheckBox chckbxIncluirSubcarpetas;
+	private File file;
+	private JButton btnBuscar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BuscadorArchivos3 frame = new BuscadorArchivos3();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public BuscadorArchivos3() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 603, 326);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JButton seleccionCarpeta = new JButton("Seleccionar Carpeta");
+		seleccionCarpeta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser jfc = new JFileChooser("/home/alumno");
+				jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int opcion = jfc.showOpenDialog(null);
+				if (opcion == JFileChooser.APPROVE_OPTION) {
+					file = jfc.getSelectedFile();
+					pathText.setText(file.getAbsolutePath());
+					btnBuscar.setEnabled(true);
+				}
+
+			}
+		});
+		seleccionCarpeta.setBounds(0, 6, 187, 27);
+		contentPane.add(seleccionCarpeta);
+
+		pathText = new JTextField();
+		pathText.setEditable(false);
+		pathText.setBounds(206, 6, 378, 27);
+		contentPane.add(pathText);
+		pathText.setColumns(10);
+
+		JLabel lblTamao = new JLabel("Patrón nombre");
+		lblTamao.setBounds(0, 59, 123, 15);
+		contentPane.add(lblTamao);
+
+		textField = new JTextField();
+		textField.setBounds(108, 53, 100, 27);
+		contentPane.add(textField);
+		textField.setColumns(10);
+
+		chckbxOcultos = new JCheckBox("Mostrar Ocultos");
+		chckbxOcultos.setBounds(331, 57, 150, 18);
+		contentPane.add(chckbxOcultos);
+
+		chckbxIncluirSubcarpetas = new JCheckBox("Incluir Subcarpetas");
+		chckbxIncluirSubcarpetas.setBounds(331, 87, 248, 18);
+		contentPane.add(chckbxIncluirSubcarpetas);
+
+		btnBuscar = new JButton("BUSCAR");
+		btnBuscar.setEnabled(false);
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+				ArrayList<File> encuentros = buscarArchivosPorNombre(file, textField.getText(),
+						chckbxOcultos.isSelected(), chckbxIncluirSubcarpetas.isSelected());
+				String texto = "";
+				for (File f : encuentros) {
+					texto += f.getPath() + "\n";
+				}
+				textArea.setText(texto);
+				} catch (PatternSyntaxException pse) {
+					JOptionPane.showInputDialog(null, "Tipo de patrón inválido\n\n\n" + pse.getClass() + ": " + pse.getMessage(),
+							"Ha ocurrido una excepción", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+
+		});
+		btnBuscar.setBounds(484, 59, 100, 27);
+		contentPane.add(btnBuscar);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(6, 135, 589, 159);
+		contentPane.add(scrollPane);
+
+		textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		textArea.setEditable(false);
+	}
+
+	ArrayList<File> buscarArchivosPorNombre(File carpetaDondeBuscar, String patronNombre, boolean incluirOcultos,
+			boolean incluirSubcarpetas) {
+		if (!carpetaDondeBuscar.isDirectory())
+			return null;
+		ArrayList<File> resul = new ArrayList<>();
+		for (File f : carpetaDondeBuscar.listFiles()) {
+			if (f.isFile()) {
+				if ((f.isHidden() && incluirOcultos) || (!f.isHidden())) {
+					if(f.getName().matches(patronNombre))
+						resul.add(f);
+				}
+			} else {
+				if ((f.isDirectory() && incluirSubcarpetas) && ((f.isHidden() && incluirOcultos) || (!f.isHidden())))
+					resul.addAll(buscarArchivosPorNombre(f, patronNombre, incluirOcultos, incluirSubcarpetas));
+			}
+		}
+		return resul;
+	}
+
+	public JTextArea getTextArea() {
+		return textArea;
+	}
+
+	public JTextField getTextField() {
+		return textField;
+	}
+
+	public JTextField getPathText() {
+		return pathText;
+	}
+
+	public JCheckBox getChckbxOcultos() {
+		return chckbxOcultos;
+	}
+
+	public JCheckBox getChckbxIncluirSubcarpetas() {
+		return chckbxIncluirSubcarpetas;
+	}
+
+	public JButton getBtnBuscar() {
+		return btnBuscar;
+	}
+}
